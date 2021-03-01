8a4d8a91c3324018ae66a9965df1d70e43c768d6
michellebakels <michelle.bakels@gmail.com>
2021-03-01 13:10:38 -0500

    new ts project

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('Hello TypeScript');
+var num1 = 5;
+var num2 = 10;
+var addNumbers = function (number1, number2) {
+    return number1 + number2;
+};
+console.log(addNumbers(num1, num2));
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.log('Hello TypeScript')
+
+let num1 = 5
+let num2 = 10
+
+const addNumbers = (number1: number, number2: number) => {
+    return number1 + number2
+}
+
+console.log(addNumbers(num1, num2))
