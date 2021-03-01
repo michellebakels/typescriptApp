4033aae78b5d9e429c5d810e7709b922d8a199f0
michellebakels <michelle.bakels@gmail.com>
2021-03-01 14:22:40 -0500

    object typing

--- build/index.js
+++ build/index.js
@@ -1,8 +1,21 @@
 "use strict";
 console.log('Hello TypeScript');
-var num1 = 5;
+var num1 = 50;
 var num2 = 10;
 var addNumbers = function (number1, number2) {
     return number1 + number2;
 };
 console.log(addNumbers(num1, num2));
+var dog = {
+    name: 'Admiral',
+    age: undefined,
+    good: true,
+    // desc: {
+    //     breed: 'Australian Shepherd',
+    //     color: 'Red Merle'
+    // }
+};
+if (dog.desc) {
+    dog.desc.breed = 'Labrador';
+}
+console.log(dog);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
 console.log('Hello TypeScript')
 
-let num1 = 5
+let num1 = 50
 let num2 = 10
 
-const addNumbers = (number1: number, number2: number) => {
+const addNumbers = (number1: number , number2: number) => {
     return number1 + number2
 }
 
 console.log(addNumbers(num1, num2))
+
+type dogType = {
+    name: string,
+    age: number | undefined,
+    good: true,
+    desc?: {
+        breed: string,
+        color: string,
+    }
+}
+
+let dog: dogType = {
+    name: 'Admiral',
+    age: undefined,
+    good: true,
+    // desc: {
+    //     breed: 'Australian Shepherd',
+    //     color: 'Red Merle'
+    // }
+}
+
+if (dog.desc) {
+    dog.desc.breed = 'Labrador'
+}
+
+console.log(dog)
